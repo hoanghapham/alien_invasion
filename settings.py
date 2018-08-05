3ccb8483e69ffe8be95806b0ce1d8c0a09bc534b
hoanghapham <hoangha.20021992@gmail.com>
2018-08-05 10:26:49 +0700

    Add button class

--- settings.py
+++ settings.py
@@ -14,8 +14,8 @@ class Settings():
 
         # Bullet settings
         self.bullet_speed_factor = 3
-        self.bullet_width = 100
-        self.bullet_height = 50
+        self.bullet_width = 10
+        self.bullet_height = 55
         self.bullet_color = 60, 60, 60
         self.bullets_allowed = 3
 
@@ -23,7 +23,7 @@ class Settings():
         self.alien_speed_factor = 1
         
         # Fleet settings
-        self.fleet_drop_speed = 50
+        self.fleet_drop_speed = 10
         self.fleet_direction = 1 # 1 means right, -1 means left
         
         # Stats
